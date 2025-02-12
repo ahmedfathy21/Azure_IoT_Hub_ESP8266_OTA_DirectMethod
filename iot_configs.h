6875be99376aa873859b1b5f0640a1a24f91dfba
Ahmed Fathy <ahmedfathy11876@gmail.com>
2025-02-12 15:07:47 +0200

    FOTA update From the Azure iot hub with direct Method v.1

--- iot_configs.h
+++ iot_configs.h
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: MIT
 
 // Wifi
-#define IOT_CONFIG_WIFI_SSID "ssid"
-#define IOT_CONFIG_WIFI_PASSWORD " password" "
+#define IOT_CONFIG_WIFI_SSID "summit"
+#define IOT_CONFIG_WIFI_PASSWORD "paradiselost12"
 
 // Azure IoT
-#define IOT_CONFIG_IOTHUB_FQDN "IOT_HUB.azure-devices.net"
-#define IOT_CONFIG_DEVICE_ID "DEVICE_ID"
-#define IOT_CONFIG_DEVICE_KEY "DEVICE_KEY"
+#define IOT_CONFIG_IOTHUB_FQDN "kakashihub.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "kakashidev"
+#define IOT_CONFIG_DEVICE_KEY "O2xP51NB9/pHksXbthLB4LUKYoWHssQ+LEG97KjLOUs="
 
 // Publish 1 message every 2 seconds
 #define TELEMETRY_FREQUENCY_MILLISECS 5000
